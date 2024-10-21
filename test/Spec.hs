e17ca00b831dc1cd2d5e10b41c10d4da36b0f78f
Justas Drakšas <justas.draksas@mif.stud.vu.lt>
2024-10-21 20:27:16 +0000

    add tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -12,11 +12,44 @@ tests :: TestTree
 tests = testGroup "Tests" [unitTests]
 
 unitTests :: TestTree
-unitTests = testGroup "Lib1 tests"
-  [ testCase "List of completions is not empty" $
-      null Lib1.completions @?= False,
-    testCase "Parsing case 1 - give a better name" $
-      Lib2.parseQuery "" @?= (Left "Some error message"),
-    testCase "Parsing case 2 - give a better name" $
-      Lib2.parseQuery "o" @?= (Left "Some error message")
+unitTests = testGroup "Lib2 tests"
+  [ 
+    testCase "Parse valid nucleotide sequence" $
+      Lib2.parseNucSeq "ATGCG" @?= Right ("ATGCG", ""),
+      
+    testCase "Parse invalid nucleotide sequence" $
+      Lib2.parseNucSeq "ATB" @?= Left "Error: invalid nucleotide.",
+      
+    testCase "Parse concat query" $
+      Lib2.parseQuery "concat ATG CGT" @?= Right (Lib2.Concat "ATG" "CGT"),
+      
+    testCase "Parse complement query" $
+      Lib2.parseQuery "complement ATG" @?= Right (Lib2.Complement "ATG"),
+      
+    testCase "Parse transcribe query" $
+      Lib2.parseQuery "transcribe ATG" @?= Right (Lib2.Transcribe "ATG"),
+      
+    testCase "Parse mutate query" $
+      Lib2.parseQuery "mutate ATG 50" @?= Right (Lib2.Mutate "ATG" 50),
+      
+    testCase "Parse invalid command" $
+      Lib2.parseQuery "invalid ATG" @?= Left "Error: Unknown command.",
+      
+    testCase "State transition - concat sequences" $
+      let state = Lib2.emptyState
+          query = Lib2.Concat "ATG" "CGT"
+      in Lib2.stateTransition state query @?= Right (Just "Concatenated: ATGCGT", Lib2.State "ATGCGT"),
+      
+    testCase "State transition - complement sequence" $
+      let state = Lib2.emptyState
+          query = Lib2.Complement "ATG"
+      in Lib2.stateTransition state query @?= Right (Just "Complement: TAC", Lib2.State "TAC"),
+      
+    testCase "State transition - transcribe sequence" $
+      let state = Lib2.emptyState
+          query = Lib2.Transcribe "ATG"
+      in Lib2.stateTransition state query @?= Right (Just "Transcribed: AUG", Lib2.State "AUG"),
+      
+    testCase "Parse with parentheses - nested operations" $
+      Lib2.parseQuery "mutate (concat ATG (complement G)) 50" @?= Right "ATGC"
   ]
