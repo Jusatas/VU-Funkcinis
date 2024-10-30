035eadfe42e6cbac6908bdd06c70cc74fa472cb1
Justas Drakšas <justas.draksas@mif.stud.vu.lt>
2024-10-30 21:36:31 +0000

    update parsers to match new pattern

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -6,35 +6,54 @@ module Lib2
       --stateTransition
     ) where
 
-data Query = Concat String String
-           | FMotif String String
-           | Complement String
-           | Transcribe String
-           | Mutate String Integer
+data Query = Concat Operand Operand
+           | FMotif Operand Operand
+           | Complement Operand
+           | Transcribe Operand
+           | Mutate Operand Integer
            deriving (Show, Eq)
 
-charParser :: Char -> String -> Either String (Char, String)
-charParser c [] = Left ("Cannot find " ++ [c] ++ " in an empty list")
-charParser c (h:t)
-  | c == h    = Right (c, t)
-  | otherwise = Left ([c] ++ " is not the first element of " ++ t)
+data Operand = Sequence [Nucleotide]
+             | Query
+             deriving (Show, Eq)
 
-stringParser :: String -> String -> Either String String
-stringParser [] input = Right input
-stringParser (h:t) input =
-  case charParser h input of
+data Nucleotide = A | T | U | C | G
+  deriving (Show, Eq)
+
+type Parser a = String -> Either String (a, String)
+
+-- parseChar :: Char -> Parser Char
+
+
+parseChar :: Char -> Parser Char
+parseChar c [] = Left ("Cannot find " ++ [c] ++ " in an empty input")
+parseChar c (h:t)
+  | c == h    = Right (c, t)
+  | otherwise = Left ([c] ++ " is not the first element of " ++ (h:t))
+
+
+
+parseString :: String -> Parser String
+parseString [] input = Right ([], input)
+parseString (h:t) input = 
+  case parseChar h input of
     Left err -> Left err
-    Right (_, rest) -> stringParser t rest
+    Right (_, rest) -> 
+      case parseString t rest of
+        Left err -> Left err
+        Right (parsed, remaining) -> Right (h:parsed, remaining)
+
+
 
 -- <integer> ::= "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | <integer> <integer>
-parseInt :: String -> Either String Integer
+parseInt :: Parser Integer
 parseInt [] = Left "Error: no digits found."
 parseInt input = parseDigits 0 input
 
 -- | Helper for `parseInt`.
-parseDigits :: Integer -> String -> Either String Integer
-parseDigits acc [] = Right (acc)
-parseDigits acc (h:t) 
+parseDigits :: Integer -> String -> Either String (Integer, String)
+parseDigits acc [] = Right (acc, [])  -- No more input; return the accumulated integer and an empty remainder
+parseDigits acc (h:t)
   | h == '0' = parseDigits (acc * 10 + 0) t
   | h == '1' = parseDigits (acc * 10 + 1) t
   | h == '2' = parseDigits (acc * 10 + 2) t
@@ -45,95 +64,111 @@ parseDigits acc (h:t)
   | h == '7' = parseDigits (acc * 10 + 7) t
   | h == '8' = parseDigits (acc * 10 + 8) t
   | h == '9' = parseDigits (acc * 10 + 9) t
-  | otherwise = Right (acc) -- Stop when a non-digit character is found
+  | otherwise = Right (acc, h:t)  -- Return the accumulated integer and the remaining unparsed input
 -- <nucleotide> ::= "A" | "T" | "C" | "G"
-parseNucleotide :: Char -> Either String Char
-parseNucleotide 'A' = Right 'A'
-parseNucleotide 'T' = Right 'T'
-parseNucleotide 'U' = Right 'U'
-parseNucleotide 'C' = Right 'C'
-parseNucleotide 'G' = Right 'G'
+parseNucleotide :: Char -> Either String Nucleotide
+parseNucleotide 'A' = Right A
+parseNucleotide 'T' = Right T
+parseNucleotide 'U' = Right U
+parseNucleotide 'C' = Right C
+parseNucleotide 'G' = Right G
 parseNucleotide _   = Left "Error: invalid nucleotide."
 
 -- | Parses a nucleotide sequence (string of valid nucleotides).
--- >>> parseNucSeq "ATG"
--- Right ("ATG","")
+-- >>> parseNucSeq "ATGx"
+-- WAS WAS Right ("ATG","")
+-- WAS NOW Right ([A,T,G],"")
+-- NOW Right ([A,T,G],"")
 
-parseNucSeq :: String -> Either String (String, String)
+
+parseNucSeq :: Parser [Nucleotide]
 parseNucSeq [] = Right ([], [])
-parseNucSeq (h:t)
-  | h == ' '  = Right ([], h:t)
-  | otherwise = case parseNucleotide h of
-      Left err -> Left err
-      Right nucleotide -> case parseNucSeq t of
-          Left err -> Left err
-          Right (nucSeq, remainder) -> Right (nucleotide : nucSeq, remainder)
+parseNucSeq (h:t) = case parseNucleotide h of
+    Left _ -> Right ([], h:t)  -- Stop parsing and return remaining input if not a nucleotide
+    Right nucleotide -> case parseNucSeq t of
+        Left err -> Left err
+        Right (nucSeq, remainder) -> Right (nucleotide : nucSeq, remainder)
+
+
+
 
 -- <operation> ::= "concat" <operand> <operand>
-parseConcat :: String -> Either String Query
-parseConcat input =
-  case stringParser "concat " input of
+parseConcat :: Parser Query
+parseConcat input = 
+  case parseString "concat " input of
     Left err -> Left err
-    Right rest1 -> case parseNucSeq rest1 of
+    Right (_, rest1) -> case parseNucSeq rest1 of
       Left err -> Left err
-      Right (seq1, rest2) -> case stringParser " " rest2 of
+      Right (seq1, rest2) -> case parseChar ' ' rest2 of
         Left err -> Left err
-        Right rest3 -> case parseNucSeq rest3 of
+        Right (_, rest3) -> case parseNucSeq rest3 of
           Left err -> Left err
-          Right (seq2, _) -> Right (Concat seq1 seq2)
+          Right (seq2, remaining) -> Right (Concat (Sequence seq1) (Sequence seq2), remaining)
+
 
 -- <operation> ::= "find-motif" <operand> <operand>
-parseFMotif :: String -> Either String Query
+
+parseFMotif :: Parser Query
 parseFMotif input =
-  case stringParser "fmotif " input of
+  case parseString "fmotif " input of
     Left err -> Left err
-    Right rest1 -> case parseNucSeq rest1 of
+    Right (_, rest1) -> case parseNucSeq rest1 of
       Left err -> Left err
-      Right (seq1, rest2) -> case stringParser " " rest2 of
+      Right (seq1, rest2) -> case parseString " " rest2 of
         Left err -> Left err
-        Right rest3 -> case parseNucSeq rest3 of
+        Right (_, rest3) -> case parseNucSeq rest3 of
           Left err -> Left err
-          Right (seq2, _) -> Right (FMotif seq1 seq2)
+          Right (seq2, remaining) -> Right (FMotif (Sequence seq1) (Sequence seq2), remaining)
+
 
 -- <operation> ::= "complement" <operand>
-parseComplement :: String -> Either String Query
+parseComplement :: Parser Query
 parseComplement input =
-  case stringParser "complement " input of
+  case parseString "complement " input of
     Left err -> Left err
-    Right rest -> case parseNucSeq rest of
+    Right (_, rest) -> case parseNucSeq rest of
       Left err -> Left err
-      Right (seq1, _) -> Right (Complement seq1)
+      Right (seq1, remaining) -> Right (Complement (Sequence seq1), remaining)
 
 -- <operation> ::= "transcribe" <operand>
-parseTranscribe :: String -> Either String Query
+parseTranscribe :: Parser Query
 parseTranscribe input =
-  case stringParser "transcribe " input of
+  case parseString "transcribe " input of
     Left err -> Left err
-    Right rest -> case parseNucSeq rest of
+    Right (_, rest) -> case parseNucSeq rest of
       Left err -> Left err
-      Right (seq1, _) -> Right (Transcribe seq1)
+      Right (seq1, remaining) -> Right (Transcribe (Sequence seq1), remaining)
+
 
 -- <operation> ::= "mutate" <operand> <percentage>
-parseMutate :: String -> Either String Query
+
+parseMutate :: Parser Query
 parseMutate input =
-  case stringParser "mutate " input of
+  case parseString "mutate " input of
     Left err -> Left err
-    Right rest1 -> case parseNucSeq rest1 of
+    Right (_, rest1) -> case parseNucSeq rest1 of
       Left err -> Left err
-      Right (seq1, rest2) -> case stringParser " " rest2 of
+      Right (seq1, rest2) -> case parseString " " rest2 of
         Left err -> Left err
-        Right rest3 -> case parseInt rest3 of
+        Right (_, rest3) -> case parseInt rest3 of
           Left err -> Left err
-          Right int -> Right (Mutate seq1 int)
+          Right (int, remaining) -> Right (Mutate (Sequence seq1) int, remaining)
 
 -- >>> parseQuery "mutate GGC (fmotif GGCCGC GC)"
--- Right "GGC"
+-- /workspaces/fp-2024/src/Lib2.hs:236:63: error:
+--     • Couldn't match expected type ‘Query’
+--                   with actual type ‘(Query, String)’
+--     • In the first argument of ‘Right’, namely ‘result’
+--       In the expression: Right result
+--       In a case alternative: Right result -> Right result
+-- (deferred type error)
 
 
 
 -- Right "GCC"
 -- >>> parseQuery "mutate (concat CC (complement G)) 50"
 -- Right "GCC"
+
 
 
 
