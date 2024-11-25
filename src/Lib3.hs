332b9231f81465f23aaee91ae465d7ce9f3c9fe4
Justas Drakšas <justas.draksas@mif.stud.vu.lt>
2024-11-25 14:37:25 +0000

    Implement state marshalling

--- src/Lib3.hs
+++ src/Lib3.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE InstanceSigs #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use list comprehension" #-}
 module Lib3
     ( stateTransition,
     StorageOp (..),
@@ -10,10 +12,9 @@ module Lib3
     ) where
 
 import Control.Concurrent ( Chan )
-import Control.Concurrent.STM(STM, TVar)
+import Control.Concurrent.STM(STM, TVar, readTVar, atomically, writeTVar)
 import qualified Lib2
 import Debug.Trace (trace)
-
 
 data StorageOp = Save String (Chan ()) | Load (Chan String)
 -- | This function is started from main
@@ -99,24 +100,21 @@ isWhitespace c = c `elem` [' ', '\n', '\t', ';']
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 -- | Converts program's state into Statements
 -- (probably a batch, but might be a single query)
 marshallState :: Lib2.State -> Statements
-marshallState _ = error "Not implemented 4"
+marshallState state = Batch queries
+  where
+    currentSeqQuery = if null (Lib2.nucleotideSequence state)
+                     then []
+                     else [Lib2.CreateSeq (Lib2.nucleotideSequence state) "current"]
+    
+    -- Create queries for all named sequences
+    namedSeqQueries = map (\(name, seq) -> Lib2.CreateSeq seq name) 
+                         (Lib2.namedSequences state)
+    
+    -- Combine all queries
+    queries = currentSeqQuery ++ namedSeqQueries
 
 -- | Renders Statements into a String which
 -- can be parsed back into Statements by parseStatements
@@ -125,7 +123,63 @@ marshallState _ = error "Not implemented 4"
 -- Must have a property test
 -- for all s: parseStatements (renderStatements s) == Right(s, "")
 renderStatements :: Statements -> String
-renderStatements _ = error "Not implemented 5"
+renderStatements (Single q) = renderQuery q
+renderStatements (Batch qs) = "BEGIN\n" ++ 
+                             concatMap (\q -> renderQuery q ++ ";\n") qs ++ 
+                             "END\n"
+
+renderQuery :: Lib2.Query -> String
+renderQuery (Lib2.CreateSeq nucleotides name) = 
+    "createSeq " ++ nucleotidesToString nucleotides ++ " " ++ name
+renderQuery (Lib2.SaveTo name query) = 
+    "saveTo " ++ name ++ " " ++ renderQuery query
+renderQuery (Lib2.Concat op1 op2) = 
+    "concat " ++ renderOperand op1 ++ " " ++ renderOperand op2
+renderQuery (Lib2.Complement op) = 
+    "complement " ++ renderOperand op
+renderQuery (Lib2.Transcribe op) = 
+    "transcribe " ++ renderOperand op
+renderQuery (Lib2.Mutate op n) = 
+    "mutate " ++ renderOperand op ++ " " ++ show n
+renderQuery Lib2.ViewCommand = "view"
+renderQuery (Lib2.DeleteSeq name) = "deleteSeq " ++ name
+renderQuery (Lib2.FMotif op1 op2) = 
+    "fmotif " ++ renderOperand op1 ++ " " ++ renderOperand op2
+
+renderOperand :: Lib2.Operand -> String
+renderOperand (Lib2.Sequence nucleotides) = nucleotidesToString nucleotides
+renderOperand (Lib2.NestedQuery query) = "(" ++ renderQuery query ++ ")"
+renderOperand (Lib2.NamedSequence name) = name
+
+nucleotidesToString :: [Lib2.Nucleotide] -> String
+nucleotidesToString = map nucleotideToChar
+  where
+    nucleotideToChar Lib2.A = 'A'
+    nucleotideToChar Lib2.T = 'T'
+    nucleotideToChar Lib2.U = 'U'
+    nucleotideToChar Lib2.C = 'C'
+    nucleotideToChar Lib2.G = 'G'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 -- | Updates a state according to a command.
 -- Performs file IO via ioChan if needed.
@@ -139,4 +193,32 @@ renderStatements _ = error "Not implemented 5"
 -- is stored in transactinal variable
 stateTransition :: TVar Lib2.State -> Command -> Chan StorageOp ->
                    IO (Either String (Maybe String))
-stateTransition _ _ ioChan = return $ Left "Not implemented 6"
+stateTransition stateVar command _ = case command of
+    LoadCommand -> return $ Left "Load not implemented yet"
+    SaveCommand -> return $ Left "Save not implemented yet"
+    StatementCommand stmts -> atomically $ do
+        state <- readTVar stateVar
+        case executeStatements state stmts of
+            Right (msg, newState) -> do
+                writeTVar stateVar newState
+                return $ Right msg
+            Left err -> return $ Left err
+
+executeStatements :: Lib2.State -> Statements -> Either String (Maybe String, Lib2.State)
+executeStatements state (Single query) = Lib2.stateTransition state query
+executeStatements state (Batch queries) = 
+    foldr combineBatchResults (Right (Nothing, state)) queries
+  where
+    combineBatchResults query acc = case acc of
+        Left err -> Left err
+        Right (_, curState) -> case Lib2.stateTransition curState query of
+            Left err -> Left err
+            Right (msg, newState) -> Right (msg, newState)
+
+        
+-- >>> marshallState Lib2.emptyState
+-- Batch []
+
+-- >>> let testState = Lib2.emptyState { Lib2.nucleotideSequence = [Lib2.A, Lib2.T, Lib2.C], Lib2.namedSequences = [("test1", [Lib2.G, Lib2.C]), ("test2", [Lib2.A, Lib2.A])]}
+-- >>> marshallState testState
+-- Batch [CreateSeq [A,T,C] "current",CreateSeq [G,C] "test1",CreateSeq [A,A] "test2"]
